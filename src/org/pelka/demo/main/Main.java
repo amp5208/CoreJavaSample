541e65172a376421ecdc13ab130f697044207b24
arnoldpelka <arnoldpelka@gmail.com>
2015-08-01 19:58:40 -0400

    Updated code to work

---
+++ src/org/pelka/demo/main/Main.java
@@ -0,0 +1,23 @@
+package org.pelka.demo.main;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.pelka.demo.util.NumberParser;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String x = getHelloStatement(args[0]);
+		NumberParser nparse = new NumberParser();
+		List<String> list = new ArrayList<String>();
+		System.out.println(x);
+		
+		int y = nparse.getIntValue(args[1]);
+	}
+	
+	private static String getHelloStatement(String x) {
+		System.out.println("TRACE: [" + x + "]");
+		return x;
+	}
+
+}
---
+++ src/org/pelka/demo/util/NumberParser.java
@@ -0,0 +1,13 @@
+package org.pelka.demo.util;
+
+public class NumberParser {
+	private int canonicalValue;
+	
+	public NumberParser () {
+		
+	}
+	
+	public int getIntValue(String x) {
+		return 0;
+	}
+}
